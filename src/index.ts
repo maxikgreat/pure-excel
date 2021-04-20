bbbff8df3450490de58ed6124d6abfdfad949e60
Maksym Vasylenko <maksym.vasylenko@polcode.net>
2021-04-20 20:32:35 +0200

    Configured Eslint, webpack

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,12 @@
-console.log('h1');
+console.log('red');
+import './scss/index.scss';
+// eslint-disable-next-line no-unused-vars
 const a: number = 25;
+
+async function start() {
+  await Promise.resolve();
+}
+
+start();
 
 export {};
--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,37 @@
 const path = require('path');
 
 // webpack plugins
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
+
+const isProd = process.env.NODE_ENV === 'production';
+const isDev = !isProd;
+
+const filename = (ext) => isDev ?
+  `bundle.${ext}` :
+  `bundle.[contenthash].${ext}`;
 
 module.exports = {
   context: path.resolve(__dirname, 'src'),
   mode: 'development',
-  devtool: 'source-map',
+  devtool: isDev ? 'source-map' : false,
+  devServer: {
+    port: '3000',
+    hot: isDev,
+  },
   resolve: {
     extensions: ['.ts', '.js'],
     alias: {
       '@': path.resolve(__dirname, 'src'),
-      '@core': path.resolve(__dirname, 'src/core')
-    }
+      '@core': path.resolve(__dirname, 'src/core'),
+    },
   },
-  entry: './index.ts',
+  entry: ['@babel/polyfill', './index.ts'],
   output: {
-    filename: 'bundle.[hash].js',
+    filename: filename('js'),
     path: path.resolve(__dirname, 'dist'),
   },
   module: {
@@ -27,25 +39,41 @@ module.exports = {
       {
         test: /\.ts$/,
         exclude: /node_modules/,
-        use: 'babel-loader'
-      }
-    ]
+        use: 'babel-loader',
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+    ],
   },
   plugins: [
     new CleanWebpackPlugin(),
     new HTMLWebpackPlugin({
-      template: 'index.html'
+      template: 'index.html',
+      minify: {
+        removeComments: isProd,
+        collapseWhitespace: isProd,
+      },
     }),
     new CopyWebpackPlugin({
       patterns: [
         {
           from: path.resolve(__dirname, 'src/favicon.ico'),
           to: path.resolve(__dirname, 'dist'),
-        }
-      ]
+        },
+      ],
     }),
     new MiniCssExtractPlugin({
-      filename: 'bundle.[hash].css'
-    })
-  ]
+      filename: filename('css'),
+    }),
+    new ESLintPlugin({
+      extensions: ['.ts'],
+      exclude: 'node_modules',
+    }),
+  ],
 };
