a790ebb5802f8084fcafc0aff8b6a8175385ed40
Maksym Vasylenko <maksym.vasylenko@polcode.net>
2021-04-20 17:33:35 +0200

    Webpack plugins added

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,25 @@
 const path = require('path');
 
+// webpack plugins
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 module.exports = {
   context: path.resolve(__dirname, 'src'),
   mode: 'development',
   devtool: 'source-map',
   resolve: {
-    extensions: ['ts', 'js']
+    extensions: ['.ts', '.js'],
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '@core': path.resolve(__dirname, 'src/core')
+    }
   },
   entry: './index.ts',
   output: {
-    filename: 'bundle.js',
+    filename: 'bundle.[hash].js',
     path: path.resolve(__dirname, 'dist'),
   },
   module: {
@@ -20,5 +30,22 @@ module.exports = {
         use: 'babel-loader'
       }
     ]
-  }
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HTMLWebpackPlugin({
+      template: 'index.html'
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'src/favicon.ico'),
+          to: path.resolve(__dirname, 'dist'),
+        }
+      ]
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'bundle.[hash].css'
+    })
+  ]
 };
