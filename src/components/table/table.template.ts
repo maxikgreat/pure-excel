7cd264808a259867bf4588d085da7d0a90f8a440
Maksym Vasylenko <maksym.vasylenko@polcode.net>
2021-05-24 10:39:29 +0200

    Complete with createTable template

--- src/components/table/table.template.ts
+++ src/components/table/table.template.ts
@@ -1,30 +1,53 @@
-
 const CODES = {
   A: 65,
   Z: 90,
 };
 
+/**
+ * return a plain empty excel cell
+ *
+ * @return {string} cell template
+ */
 const createCell = (): string => {
   return `
-    <div class="cell" contenteditable>B2</div>
+    <div class="cell" contenteditable></div>
   `;
 };
 
+/**
+ * return an excel column with A, B, C, D, E values
+ *
+ * @param {string} elem
+ * @return {string} column template
+ */
 const createCol = (elem: string): string => {
   return `
     <div class="column">${elem}</div>
   `;
 };
 
-const createRow = (content?: string): string => {
+/**
+ * return an excel row with info (numbers) and content (cells)
+ *
+ * @param {string} content will be inserted in 'row-data' div
+ * @param {number} index will be inserted in 'row-info' div if exists
+ * @return {string} row template
+ */
+const createRow = (content: string, index?: number): string => {
   return `
     <div class="row">
-      <div class="row-info"></div>
+      <div class="row-info">${index ?? ''}</div>
       <div class="row-data">${content}</div>
     </div>
   `;
 };
 
+/**
+ * return a part of a row
+ *
+ * @param {Function} callback for each iteration
+ * @return {string} line of columns template
+ */
 const createColsArray = (callback: Function): string =>
   new Array(CODES.Z - CODES.A + 1)
       .fill('')
@@ -37,12 +60,12 @@ export default function(rowsCount: number): string {
   const rows: string[] = [];
 
   const cols = createColsArray(createCol);
-  const cells = createColsArray(createCell);
 
   rows.push(createRow(cols));
 
   for (let i = 0; i < rowsCount; i++) {
-    rows.push(createRow(cells));
+    const cells = createColsArray(createCell);
+    rows.push(createRow(cells, i + 1));
   }
 
   return rows.join('');
