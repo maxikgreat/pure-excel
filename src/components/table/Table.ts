3edad1eba702deb0fa52e57bdb5060a10246387f
Maksym Vasylenko <maksym.vasylenko@polcode.net>
2021-05-25 15:50:33 +0200

    Add resize handlers and add meta data

--- src/components/table/Table.ts
+++ src/components/table/Table.ts
@@ -1,11 +1,27 @@
 import ExcelComponent from '@core/ExcelComponent';
+import resizeHandler from './table.resize';
 import createTable from './table.template';
+
+interface MouseEventOnDiv extends MouseEvent {
+  target: HTMLDivElement | null
+}
 
 class Table extends ExcelComponent {
   static className = 'excel__table';
 
+  constructor($root: HTMLElement) {
+    super($root, {
+      name: 'Table',
+      listeners: ['mousedown'],
+    });
+  }
+
   public toHTML(): string {
     return createTable(20);
+  }
+
+  protected onMousedown({target}: MouseEventOnDiv): void {
+    resizeHandler(this.$root, target);
   }
 }
 
---
+++ src/components/table/table.resize.ts
@@ -0,0 +1,47 @@
+const resizeHandler = ($root: HTMLElement, target: HTMLDivElement | null): void => {
+  if (!target) return;
+
+  const parent = target.closest<HTMLDivElement>('[data-type="resizable"]');
+  if (!parent) return;
+
+  const resize = target.dataset?.resize as 'row' | 'column' | undefined;
+  if (!resize) return;
+
+  const colIndex = parent.dataset?.column as string;
+  const {width, height, right, bottom} = parent.getBoundingClientRect();
+
+  target.style.opacity = '1';
+  target.style[resize === 'column' ? 'bottom' : 'right'] = '-5000px';
+
+  let delta: number;
+
+  document.onmousemove = ({pageX, pageY}) => {
+    if (resize === 'column') {
+      delta = pageX - right;
+      target.style.right = `${-delta}px`;
+    } else {
+      delta = pageY - bottom;
+      target.style.bottom = `${-delta}px`;
+    }
+  };
+
+  document.onmouseup = () => {
+    if (resize === 'column') {
+      $root.querySelectorAll<HTMLDivElement>(`[data-column="${colIndex}"]`)
+          .forEach((cell) =>
+            cell.style.width = `${width + delta}px`,
+          );
+    } else {
+      parent.style.height = `${height + delta}px`;
+    }
+
+    document.onmousemove = null;
+    document.onmouseup = null;
+
+    target.style.opacity = '0';
+    target.style.bottom = '0';
+    target.style.right = '0';
+  };
+};
+
+export default resizeHandler;
